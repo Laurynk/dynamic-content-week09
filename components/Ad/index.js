a3db26e11b400903029b2a3ed347e288bf0e6032
Lauryn Kung <lauryn@Lauryns-MacBook-Pro.local>
2023-03-02 14:07:50 -0800

    initial commit

---
+++ components/Ad/index.js
@@ -0,0 +1,21 @@
+import styles from './Ad.module.css'
+import {useState} from "react"
+
+export default function Ad() {
+
+    const [popup, setPopup] = useState(true);
+    return(
+        <>
+            {
+                popup ? 
+                    <div className={styles.container}>
+                        <div className={styles.popup_area}>
+                            <div onClick={() => setPopup(false)}>close</div>
+                            <h1>Welcome</h1>
+                        </div>
+                    </div>
+                    : <></>
+            }
+        </>
+    )
+}
